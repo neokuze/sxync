14f1e3067682b37db84df91dc70a71ff0c4bce96
Yado <imyadiel@gmail.com>
2023-07-12 14:18:07 -0400

    Add files via upload

--- sxync/__init__.py
+++ sxync/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1-4a"
+__version__ = "0.0.1-5a"
 from . import client
 from . import connection
 from . import room_events
--- sxync/room.py
+++ sxync/room.py
@@ -19,12 +19,12 @@ class Room(WS):
         return self._name
     
     async def on_connect(self):
-        await self._ws.send_json({"command":"get_userlist","kwargs":{"target":self.name}})
-        await self._ws.send_json({"command":"get_history","kwargs":{"target":self.name}})
+        await self._send_command({"command":"get_userlist","kwargs":{"target":self.name}})
+        await self._send_command({"command":"get_history","kwargs":{"target":self.name}})
         await self._client._call_event("connect", self)
         
     async def send_msg(self, text):
-        await self._ws.send_json({"command":"message","kwargs":{"text":text,"target":self.name}})
+        await self._send_command({"command":"message","kwargs":{"text":text,"target":self.name}})
         
     def find_user_by_id(self, args):
         return [(x.id,x.name) for x in self._userlist if x.id == int(args)]
