b6687c052cfa76412a317f3f2e9c1a4a2efdbad7
Yado <imyadiel@gmail.com>
2024-03-14 06:25:59 -0400

    better organization, added constants.py, and other
    added reuse of cookies (not tested)
    added constants to eliminate innecesary coding.
    updated listen_websocket to let aiohttp take control.
    creating a specific function to make a request and valid a room.
    added eventhandler to reduce space in client.
    better looking to _connect

--- sxync/utils.py
+++ sxync/utils.py
@@ -1,3 +1,9 @@
+import random
+import string
+import base64
+
+from . import constants
+from bs4 import BeautifulSoup
 
 def cleanText(text):
     """Regresa texto en minúsculas y sin acentos :> thx linkkg"""
@@ -15,3 +21,34 @@ def public_attributes(obj):
     return [
         x for x in set(list(obj.__dict__.keys()) + list(dir(type(obj)))) if x[0] != "_"
     ]
+    
+def generate_header():
+    key = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16)).encode('utf-8')
+    headers = {
+        'Connection': 'keep-alive, Upgrade',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Origin': f'https://{constants.url}',
+        'Sec-WebSocket-Version': '13',
+        'Sec-WebSocket-Extensions': 'permessage-deflate',
+        'Sec-WebSocket-Key': base64.b64encode(key).decode('utf-8'),
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'websocket',
+        'Sec-Fetch-Site': 'same-origin',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'Upgrade': 'websocket',
+    }
+    return headers
+    
+async def is_room_valid(session, name):
+    """
+    an ugly way to check is exist.
+    """
+    room_valid = await session.get(constants.room_url+f"{name}/", headers={'referer': constants.login_url})
+    soup = BeautifulSoup(await room_valid.text(), 'html.parser')
+    isval = soup.find('button', {'class': 'btn btn-primary'})
+    if isval == None:
+        return True
+    return False
+    
