97e2ab1fc28ce7c3ee529ee9504e7e6784149e5d
Yado <imneokuze@gmail.com>
2024-06-09 18:32:26 -0400

    Update handler.py

--- sxync/handler.py
+++ sxync/handler.py
@@ -2,9 +2,6 @@ import logging
 import inspect
 import typing
 import asyncio
-import threading
-import time
-from typing import Callable, Any, Awaitable
 
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
